13439fdc4f43d9c0ff078b721b65f277ac0fbc5d
Sheffyjoseph <92717622+Sheffyjoseph@users.noreply.github.com>
2025-12-03 20:28:10 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ShoppingCart } from "lucide-react";
+import Cards from "./components/Cards";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchdata = async () => {
+      const res = await axios.get(
+        "https://zartek-task.vercel.app/api/resto-cafe"
+      );
+      console.log(res.data.data);
+      setData(res.data.data);
+    };
+    fetchdata();
+  }, []);
 
-export default App
+  return (
+    <div className="h-[100vh]">
+      <div className="h-[15vh] bg-red-200 flex flex-col justify-center ">
+        <div className="h-[40%] flex items-center justify-between px-5 gap-5">
+          <h1 className="text-xl flex-1">UNI Resto Cafe</h1>
+          <h1>My Orders</h1>
+          <ShoppingCart />
+        </div>
+        <ul
+          className=" flex justify-around gap-6 px-5 pt-5
+          overflow-x-auto whitespace-nowrap 
+          hide-scrollbar"
+        >
+          {data[0]?.table_menu_list.map((cat, index) => (
+            <li key={index} className="">
+              {cat?.menu_category}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="h-[85vh] bg-green-100">
+        <Cards dish={data[0]?.table_menu_list[0]?.category_dishes} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
