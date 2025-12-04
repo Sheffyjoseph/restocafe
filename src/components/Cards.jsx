673b26a069138901db6ab821268468f88ed99d4f
Sheffyjoseph <92717622+Sheffyjoseph@users.noreply.github.com>
2025-12-04 23:27:35 +0530

    partial responsive

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,15 +2,11 @@ import { Disc } from "lucide-react";
 import React, { useState, useEffect } from "react";
 
 const Cards = ({ dish, cart, setCart }) => {
-  // const [cart, setCart] = useState({});
-
-  // Load cart from localStorage when component mounts
   useEffect(() => {
     const savedCart = JSON.parse(localStorage.getItem("cart")) || {};
     setCart(savedCart);
   }, []);
 
-  // Update localStorage whenever cart changes
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
@@ -54,45 +50,47 @@ const Cards = ({ dish, cart, setCart }) => {
           key={item.dish_id}
           className="h-[20vh] flex flex-row my-1 bg-white"
         >
-          {/* Index */}
-          <div className="h-full w-[2%] mt-3 "> 
-            <Disc className={`${ item.dish_Availability?"text-green-400":"text-red-400" }`}/>
-            </div>
+          <div className="h-full w-[2%] mt-3">
+            <Disc
+              className={`${
+                item.dish_Availability ? "text-green-400" : "text-red-400"
+              }`}
+            />
+          </div>
 
-          {/* Item Info */}
-          <div className="h-full w-[78%] mt-1 px-2">
+          <div className="h-full w-[68%] lg:mt-1 lg:px-2 px-5">
             <h1 className="text-lg font-semibold mt-1">{item.dish_name}</h1>
             <h1>
               {item.dish_currency} {item.dish_price}
             </h1>
-            <p className="text-sm font-sans font-thin mt-1">
+            <p className="text-sm font-sans font-thin mt-1 text-wrap">
               {item.dish_description}
             </p>
 
             {item.dish_Availability ? (
               <>
-                <div className="bg-green-500 w-28 flex flex-row justify-around items-center rounded-full mt-1 text-white">
+                <div className="bg-green-600 w-28 flex flex-row justify-around items-center rounded-full mt-1 text-white">
                   <button onClick={() => handleRemove(item)}> - </button>
                   <p>{getQuantity(item)}</p>
                   <button onClick={() => handleAdd(item)}> + </button>
                 </div>
 
-                <p className="text-red-500 mt-1">{item?.addonCat.length > 0 ? "Customization Available":""}</p>
+                <p className="text-red-500 mt-1">
+                  {item?.addonCat.length > 0 ? "Customization Available" : ""}
+                </p>
               </>
             ) : (
               <p className="text-red-500 mt-1">Not Available</p>
             )}
           </div>
 
-          {/* Calories */}
-          <div className="h-full w-[10%] mt-5 pt-5">
+          <div className="h-full lg:w-[10%] w-[25%] lg:mt-5 lg:pt-5 pt-5 text-sm">
             {item.dish_calories} Calories
           </div>
 
-          {/* Image */}
           <div className="h-full w-[20%] flex justify-center pt-4">
             <img
-              className="w-30 h-28 rounded-xl"
+              className="lg:w-30 lg:h-28 w-25 h-25 rounded-xl"
               src={item.dish_image}
               alt={item.dish_name}
             />
